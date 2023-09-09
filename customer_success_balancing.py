a0388c77403db140a8c222023cb468cdf7f390b0
Cammy92 <camilaprogramadora@gmail.com>
2023-09-08 23:08:32 -0300

    Clean code e TDD

--- customer_success_balancing.py
+++ customer_success_balancing.py
@@ -1,24 +1,40 @@
-
 def customerSuccessBalancingDistribution(customer_success_levels, customer_levels, customer_success_unavailable):
     if not customer_success_levels or not customer_levels:
         return 0
 
-    available_cs = [cs for cs in customer_success_levels if cs[0] not in customer_success_unavailable]
+    available_cs = filter_unavailable_cs(customer_success_levels, customer_success_unavailable)
 
     if not available_cs:
         return 0
 
-    cs_customers_count = {cs[0]: 0 for cs in available_cs}
+    cs_customers_count = count_customers_per_cs(available_cs, customer_levels)
+
+    if not cs_customers_count or all(count == 0 for count in cs_customers_count.values()):
+        return 0
+
+    return find_best_customer_success(cs_customers_count)
+
+
+def filter_unavailable_cs(customer_success_levels, customer_success_unavailable):
+    unavailable_ids = set(customer_success_unavailable)
+    return [cs for cs in customer_success_levels if cs["ID"] not in unavailable_ids]
+
+def count_customers_per_cs(available_cs, customer_levels):
+    cs_customers_count = {cs["ID"]: 0 for cs in available_cs}
 
     for customer in customer_levels:
-        closest_cs = min(available_cs, key=lambda cs: abs(cs[1] - customer[1]))
-        cs_customers_count[closest_cs[0]] += 1
+        closest_cs = find_closest_cs(available_cs, customer["Score"])
+        if closest_cs:
+            cs_customers_count[closest_cs["ID"]] += 1
 
+    return cs_customers_count
+
+
+def find_closest_cs(available_cs, customer_level):
+    return min(available_cs, key=lambda cs: abs(cs["Score"] - customer_level))
+
+def find_best_customer_success(cs_customers_count):
     max_customers = max(cs_customers_count.values())
     best_cs = [cs for cs, count in cs_customers_count.items() if count == max_customers]
 
-    if len(best_cs) == 1:
-        return best_cs[0]
-    else:
-        return 0
-
+    return best_cs[0] if len(best_cs) == 1 else 0
--- tests/test_customer_success_balancing.py
+++ tests/test_customer_success_balancing.py
@@ -1,35 +1,413 @@
 import unittest
 from customer_success_balancing import customerSuccessBalancingDistribution
 
+
 class TestCustomerSuccessBalancing(unittest.TestCase):
-
-    def test_case1(self):
-        customer_success_levels = [(1, 50), (2, 100), (3, 150), (4, 200)]
-        customer_levels = [(1, 20), (2, 30), (3, 35), (4, 40), (5, 60), (6, 80)]
+    def test_scenario_1(self):
+        customer_success_levels = [
+            {"ID": 1, "Score": 60},
+            {"ID": 2, "Score": 20},
+            {"ID": 3, "Score": 95},
+            {"ID": 4, "Score": 75}
+        ]
+        customer_levels = [
+            {"ID": 1, "Score": 90},
+            {"ID": 2, "Score": 20},
+            {"ID": 3, "Score": 70},
+            {"ID": 4, "Score": 40},
+            {"ID": 5, "Score": 60},
+            {"ID": 6, "Score": 10}
+        ]
         customer_success_unavailable = [2, 4]
-        result = customerSuccessBalancingDistribution(customer_success_levels, customer_levels, customer_success_unavailable)
-        self.assertEqual(result, 1)
+        expected = 1
+        result = customerSuccessBalancingDistribution(customer_success_levels, customer_levels,
+                                                      customer_success_unavailable)
+        self.assertEqual(result, expected)
 
-    def test_case2(self):
-        customer_success_levels = [(1, 50), (2, 100), (3, 150)]
-        customer_levels = [(1, 20), (2, 30), (3, 35), (4, 40), (5, 60), (6, 80)]
-        customer_success_unavailable = [2]
-        result = customerSuccessBalancingDistribution(customer_success_levels, customer_levels, customer_success_unavailable)
-        self.assertEqual(result, 1)
-
-    def test_case3(self):
-        customer_success_levels = [(1, 50), (2, 100), (3, 150)]
-        customer_levels = [(1, 20), (2, 30), (3, 35), (4, 40), (5, 60), (6, 80)]
-        customer_success_unavailable = []
-        result = customerSuccessBalancingDistribution(customer_success_levels, customer_levels, customer_success_unavailable)
-        self.assertEqual(result, 1)
-
-    def test_case4(self):
-        cs_levels = [(1, 50), (2, 100), (3, 150)]
-        customer_levels = [(1, 20), (2, 30), (3, 35), (4, 40), (5, 60), (6, 80)]
-        unavailable_cs = [1, 2, 3]
+    def test_scenario_2(self):
+        cs_levels = [
+            {"ID": 11, "Score": 60},
+            {"ID": 21, "Score": 20},
+            {"ID": 31, "Score": 95},
+            {"ID": 3, "Score": 75},
+            {"ID": 4, "Score": 10},
+            {"ID": 5, "Score": 30}
+        ]
+        customer_levels = [
+            {"ID": 10, "Score": 10},
+            {"ID": 10, "Score": 10},
+            {"ID": 10, "Score": 10},
+            {"ID": 20, "Score": 20},
+            {"ID": 20, "Score": 20},
+            {"ID": 30, "Score": 30},
+            {"ID": 30, "Score": 30},
+            {"ID": 30, "Score": 30},
+            {"ID": 20, "Score": 20},
+            {"ID": 60, "Score": 60}
+        ]
+        unavailable_cs = []
+        expected = 0
         result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
-        self.assertEqual(result, 0)
+        self.assertEqual(result, expected)
+
+    def test_scenario_3(self):
+        cs_levels = [
+            {"ID": 1, "Score": 60},
+            {"ID": 2, "Score": 20},
+            {"ID": 3, "Score": 95},
+            {"ID": 4, "Score": 75}
+        ]
+        customer_levels = [
+            {"ID": 1, "Score": 90},
+            {"ID": 2, "Score": 20},
+            {"ID": 3, "Score": 70},
+            {"ID": 4, "Score": 40},
+            {"ID": 5, "Score": 60},
+            {"ID": 6, "Score": 10}
+        ]
+        unavailable_cs = [2, 4]
+        expected = 1
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_4(self):
+        cs_levels = [
+            {"ID": 11, "Score": 60},
+            {"ID": 21, "Score": 20},
+            {"ID": 31, "Score": 95},
+            {"ID": 3, "Score": 75},
+            {"ID": 4, "Score": 10},
+            {"ID": 5, "Score": 30}
+        ]
+        customer_levels = [
+            {"ID": 10, "Score": 10},
+            {"ID": 10, "Score": 10},
+            {"ID": 10, "Score": 10},
+            {"ID": 20, "Score": 20},
+            {"ID": 20, "Score": 20},
+            {"ID": 30, "Score": 30},
+            {"ID": 30, "Score": 30},
+            {"ID": 30, "Score": 30},
+            {"ID": 20, "Score": 20},
+            {"ID": 60, "Score": 60}
+        ]
+        unavailable_cs = []
+        expected = 0
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_5(self):
+        cs_levels = [{"ID": i, "Score": i} for i in range(1, 999)]
+        customer_levels = [{"ID": i, "Score": 998} for i in range(1, 100001)]
+        unavailable_cs = [999]
+        expected = 998
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_6(self):
+        cs_levels = [{"ID": i, "Score": i} for i in range(1, 7)]
+        customer_levels = [{"ID": i, "Score": i} for i in range(1, 11)]
+        unavailable_cs = []
+        expected = 0
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_7(self):
+        cs_levels = [{"ID": 100, "Score": 100}, {"ID": 2, "Score": 2}, {"ID": 3, "Score": 3},
+                     {"ID": 6, "Score": 6}, {"ID": 4, "Score": 4}, {"ID": 5, "Score": 5}]
+        customer_levels = [{"ID": i, "Score": i} for i in range(1, 11)]
+        unavailable_cs = []
+        expected = 1
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_8(self):
+        cs_levels = [{"ID": 100, "Score": 100}, {"ID": 99, "Score": 99}, {"ID": 88, "Score": 88},
+                     {"ID": 3, "Score": 3}, {"ID": 4, "Score": 4}, {"ID": 5, "Score": 5}]
+        customer_levels = [{"ID": i, "Score": i} for i in range(1, 11)]
+        unavailable_cs = [1, 3, 2]
+        expected = 0
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_9(self):
+        cs_levels = [{"ID": 100, "Score": 100}, {"ID": 99, "Score": 99}, {"ID": 88, "Score": 88},
+                     {"ID": 3, "Score": 3}, {"ID": 4, "Score": 4}, {"ID": 5, "Score": 5}]
+        customer_levels = [{"ID": i, "Score": i} for i in range(1, 11)]
+        unavailable_cs = [4, 5, 6]
+        expected = 3
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_10(self):
+        cs_levels = [{"ID": 60, "Score": 60}, {"ID": 40, "Score": 40}, {"ID": 95, "Score": 95}, {"ID": 75, "Score": 75}]
+        customer_levels = [{"ID": 90, "Score": 90}, {"ID": 70, "Score": 70}, {"ID": 20, "Score": 20},
+                           {"ID": 40, "Score": 40}, {"ID": 60, "Score": 60}, {"ID": 10, "Score": 10}]
+        unavailable_cs = [2, 4]
+        expected = 1
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_11(self):
+        cs_levels = [
+            {"id": 1, "score": 60},
+            {"id": 2, "score": 20},
+            {"id": 3, "score": 95},
+            {"id": 4, "score": 75},
+        ]
+        customer_levels = [
+            {"id": 1, "score": 90},
+            {"id": 2, "score": 20},
+            {"id": 3, "score": 70},
+            {"id": 4, "score": 40},
+            {"id": 5, "score": 60},
+            {"id": 6, "score": 10},
+        ]
+        unavailable_cs = [2, 4]
+        expected = 1
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        assert result == expected
+
+    def test_scenario_12(self):
+        cs_levels = mapEntities([11, 21, 31, 3, 4, 5])
+        customer_levels = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60])
+        unavailable_cs = []
+        expected = 0
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        assert result == expected
+
+    def test_scenario_13(self):
+        test_timeout_in_ms = 100
+        test_start_time = time.time()
+
+        cs_levels = mapEntities(arraySeq(999, 1))
+        customer_levels = buildSizeEntities(10000, 998)
+        unavailable_cs = [999]
+        expected = 998
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        assert result == expected
+
+        if time.time() - test_start_time > test_timeout_in_ms / 1000:
+            raise Exception(f"Test took longer than {test_timeout_in_ms}ms!")
+
+    def test_scenario_14(self):
+        cs_levels = mapEntities([1, 2, 3, 4, 5, 6])
+        customer_levels = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60])
+        unavailable_cs = []
+        expected = 0
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        assert result == expected
+
+    def test_scenario_15(self):
+        cs_levels = mapEntities([100, 2, 3, 6, 4, 5])
+        customer_levels = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60])
+        unavailable_cs = []
+        expected = 1
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        assert result == expected
+
+    def test_scenario_16(self):
+        cs_levels = mapEntities([100, 99, 88, 3, 4, 5])
+        customer_levels = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60])
+        unavailable_cs = [1, 3, 2]
+        expected = 0
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        assert result == expected
+
+    def test_scenario_17(self):
+        cs_levels = mapEntities([100, 99, 88, 3, 4, 5])
+        customer_levels = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60])
+        unavailable_cs = [4, 5, 6]
+        expected = 3
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        assert result == expected
+
+    def test_scenario_18(self):
+        cs_levels = mapEntities([60, 40, 95, 75])
+        customer_levels = mapEntities([90, 70, 20, 40, 60, 10])
+        unavailable_cs = [2, 4]
+        expected = 1
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        assert result == expected
+
+    def test_scenario_19(self):
+        customer_success_levels = [
+            {"ID": 60, "Score": 60},
+            {"ID": 20, "Score": 20},
+            {"ID": 95, "Score": 95},
+            {"ID": 75, "Score": 75}
+        ]
+        customer_levels = [
+            {"ID": 90, "Score": 90},
+            {"ID": 20, "Score": 20},
+            {"ID": 70, "Score": 70},
+            {"ID": 40, "Score": 40},
+            {"ID": 60, "Score": 60},
+            {"ID": 10, "Score": 10}
+        ]
+        customer_success_unavailable = [2, 4]
+        expected = 1
+        result = customerSuccessBalancingDistribution(customer_success_levels, customer_levels,
+                                                      customer_success_unavailable)
+        self.assertEqual(result, expected)
+
+    def test_scenario_20(self):
+        cs_levels = [
+            {"ID": 11, "Score": 60},
+            {"ID": 21, "Score": 20},
+            {"ID": 31, "Score": 95},
+            {"ID": 3, "Score": 75},
+            {"ID": 4, "Score": 10},
+            {"ID": 5, "Score": 30}
+        ]
+        customer_levels = [
+            {"ID": 10, "Score": 10},
+            {"ID": 10, "Score": 10},
+            {"ID": 10, "Score": 10},
+            {"ID": 20, "Score": 20},
+            {"ID": 20, "Score": 20},
+            {"ID": 30, "Score": 30},
+            {"ID": 30, "Score": 30},
+            {"ID": 30, "Score": 30},
+            {"ID": 20, "Score": 20},
+            {"ID": 60, "Score": 60}
+        ]
+        unavailable_cs = []
+        expected = 0
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_21(self):
+        cs_levels = [{"ID": i, "Score": i} for i in range(1, 1000)]
+        customer_levels = [{"ID": i, "Score": 998} for i in range(1, 10001)]
+        unavailable_cs = [999]
+        expected = 998
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_22(self):
+        cs_levels = [
+            {"ID": 1, "Score": 1},
+            {"ID": 2, "Score": 2},
+            {"ID": 3, "Score": 3},
+            {"ID": 4, "Score": 4},
+            {"ID": 5, "Score": 5},
+            {"ID": 6, "Score": 6}
+        ]
+        customer_levels = [
+            {"ID": 1, "Score": 10},
+            {"ID": 2, "Score": 10},
+            {"ID": 3, "Score": 10},
+            {"ID": 4, "Score": 20},
+            {"ID": 5, "Score": 20},
+            {"ID": 6, "Score": 30},
+            {"ID": 7, "Score": 30},
+            {"ID": 8, "Score": 30},
+            {"ID": 9, "Score": 20},
+            {"ID": 10, "Score": 60}
+        ]
+        unavailable_cs = []
+        expected = 0
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_23(self):
+        cs_levels = [
+            {"ID": 100, "Score": 100},
+            {"ID": 2, "Score": 2},
+            {"ID": 3, "Score": 3},
+            {"ID": 6, "Score": 6},
+            {"ID": 4, "Score": 4},
+            {"ID": 5, "Score": 5}
+        ]
+        customer_levels = [
+            {"ID": 1, "Score": 10},
+            {"ID": 2, "Score": 10},
+            {"ID": 3, "Score": 10},
+            {"ID": 4, "Score": 20},
+            {"ID": 5, "Score": 20},
+            {"ID": 6, "Score": 30},
+            {"ID": 7, "Score": 30},
+            {"ID": 8, "Score": 30},
+            {"ID": 9, "Score": 20},
+            {"ID": 10, "Score": 60}
+        ]
+        unavailable_cs = []
+        expected = 1
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_24(self):
+        cs_levels = [
+            {"ID": 100, "Score": 100},
+            {"ID": 99, "Score": 99},
+            {"ID": 88, "Score": 88},
+            {"ID": 3, "Score": 3},
+            {"ID": 4, "Score": 4},
+            {"ID": 5, "Score": 5}
+        ]
+        customer_levels = [
+            {"ID": 1, "Score": 10},
+            {"ID": 2, "Score": 10},
+            {"ID": 3, "Score": 10},
+            {"ID": 4, "Score": 20},
+            {"ID": 5, "Score": 20},
+            {"ID": 6, "Score": 30},
+            {"ID": 7, "Score": 30},
+            {"ID": 8, "Score": 30},
+            {"ID": 9, "Score": 20},
+            {"ID": 10, "Score": 60}
+        ]
+        unavailable_cs = [1, 3, 2]
+        expected = 0
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_25(self):
+        cs_levels = [
+            {"ID": 100, "Score": 100},
+            {"ID": 99, "Score": 99},
+            {"ID": 88, "Score": 88},
+            {"ID": 3, "Score": 3},
+            {"ID": 4, "Score": 4},
+            {"ID": 5, "Score": 5}
+        ]
+        customer_levels = [
+            {"ID": 1, "Score": 10},
+            {"ID": 2, "Score": 10},
+            {"ID": 3, "Score": 10},
+            {"ID": 4, "Score": 20},
+            {"ID": 5, "Score": 20},
+            {"ID": 6, "Score": 30},
+            {"ID": 7, "Score": 30},
+            {"ID": 8, "Score": 30},
+            {"ID": 9, "Score": 20},
+            {"ID": 10, "Score": 60}
+        ]
+        unavailable_cs = [4, 5, 6]
+        expected = 3
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
+
+    def test_scenario_26(self):
+        cs_levels = [
+            {"ID": 60, "Score": 60},
+            {"ID": 40, "Score": 40},
+            {"ID": 95, "Score": 95},
+            {"ID": 75, "Score": 75}
+        ]
+        customer_levels = [
+            {"ID": 90, "Score": 90},
+            {"ID": 70, "Score": 70},
+            {"ID": 20, "Score": 20},
+            {"ID": 40, "Score": 40},
+            {"ID": 60, "Score": 60},
+            {"ID": 10, "Score": 10}
+        ]
+        unavailable_cs = [2, 4]
+        expected = 1
+        result = customerSuccessBalancingDistribution(cs_levels, customer_levels, unavailable_cs)
+        self.assertEqual(result, expected)
 
     if __name__ == '__main__':
         unittest.main()
